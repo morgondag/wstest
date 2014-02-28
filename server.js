7448db27d53ec7065494ebcb78d6cd0f268d4132
Morgondag <kim@morgondag.nu>
2014-03-01 00:23:24 +0100

    somekind of thing

---
+++ server.js
@@ -0,0 +1,20 @@
+var WebSocketServer = require('ws').Server;
+
+var wss = new WebSocketServer({
+	port: 1337
+});
+
+wws.on('connection', function(ws)) {
+	console.log('some dude connected');
+
+	ws.on('message', function(message) {
+		console.log(wss.port, ' Msg: ',message)
+		wss.broadcast(message)
+	});
+}
+
+wss.broadcast = function(data) {
+	for (var i in this.clients) {
+		this.clients[i].send(data);
+	}
+};
