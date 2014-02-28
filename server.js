f77c7a1df1bd61a2dfa01cdb9ed5562cf3359087
Morgondag <kim@morgondag.nu>
2014-03-01 00:35:00 +0100

    fixed fuckup
    #nightcoding

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var wss = new WebSocketServer({
 	port: 1337
 });
 
-wws.on('connection', function(ws)) {
+wws.on('connection', function(ws) {
 	console.log('some dude connected');
 
 	ws.on('message', function(message) {
 		console.log(wss.port, ' Msg: ',message)
 		wss.broadcast(message)
 	});
-}
+});
 
 wss.broadcast = function(data) {
 	for (var i in this.clients) {
